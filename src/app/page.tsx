c16197db361fa662ea6a796f654e81372327dfb1
Luis Felipe <luisrochacruzalves@gmail.com>
2025-11-08 12:10:30 -0300

    🎨 Style: Update background gradient class in main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { HydrateClient } from "~/trpc/server";
 export default async function Home() {
 	return (
 		<HydrateClient>
-			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+			<main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
 			<h1>Caicara Stock</h1>
 			</main>
 		</HydrateClient>
