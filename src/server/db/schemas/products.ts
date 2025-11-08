f4d4f9cebabb6e49ed7c6058351854dfaf1e532c
Luis Felipe <luisrochacruzalves@gmail.com>
2025-11-08 12:21:07 -0300

    ✨ Feat: Add products table schema definition

---
+++ src/server/db/schemas/products.ts
@@ -0,0 +1,11 @@
+import { createTable } from "../schema";
+
+export const products = createTable("product", (d) => ({
+	id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+	name: d.varchar({ length: 256 }).notNull(),
+	description: d.varchar({ length: 256 }),
+	price: d.numeric({ precision: 10, scale: 2 }).notNull(),
+	quantity: d.integer().notNull(),
+	createdAt: d.timestamp({ withTimezone: true }).$defaultFn(() => new Date()).notNull(),
+	updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+}));
