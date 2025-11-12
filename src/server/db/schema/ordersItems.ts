45a99918b0e11afdc78468b7b22c4ff07669e7c8
Luis Felipe <luisrochacruzalves@gmail.com>
2025-11-12 19:50:43 -0300

    🔧 Refactor: Standardize formatting in orderItems schema
    - Reformatted the `orderItems` schema for improved readability and consistency.
    - Ensured proper indentation and alignment of properties within the schema definition.
    - Maintained existing functionality while enhancing code clarity.

--- src/server/db/schema/ordersItems.ts
+++ src/server/db/schema/ordersItems.ts
@@ -1,28 +1,34 @@
 import { relations } from "drizzle-orm";
 import { createTable } from "../table";
-import { products } from "./products";
 import { orders } from "./orders";
+import { products } from "./products";
 
 export const orderItems = createTable("order_item", (d) => ({
-	id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-	orderId: d
-		.integer()
-		.notNull()
-		.references(() => orders.id, { onDelete: "cascade" }),
-	productId: d.integer().notNull().references(() => products.id),
-	quantity: d.integer().notNull(),
-	price: d.numeric({ precision: 10, scale: 2 }).notNull(),
-	createdAt: d.timestamp({ withTimezone: true }).$defaultFn(() => new Date()).notNull(),
-	updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  orderId: d
+    .integer()
+    .notNull()
+    .references(() => orders.id, { onDelete: "cascade" }),
+  productId: d
+    .integer()
+    .notNull()
+    .references(() => products.id),
+  quantity: d.integer().notNull(),
+  price: d.numeric({ precision: 10, scale: 2 }).notNull(),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .$defaultFn(() => new Date())
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
 }));
 
 export const orderItemsRelations = relations(orderItems, ({ one }) => ({
-	order: one(orders, {
-		fields: [orderItems.orderId],
-		references: [orders.id],
-	}),
-	product: one(products, {
-		fields: [orderItems.productId],
-		references: [products.id],
-	}),
+  order: one(orders, {
+    fields: [orderItems.orderId],
+    references: [orders.id],
+  }),
+  product: one(products, {
+    fields: [orderItems.productId],
+    references: [products.id],
+  }),
 }));
